2331d7705af4dd91c26d6633412f43997e971c78
Aaditya Chhetri <zetroxyyy@gmail.com>
2026-06-28 13:06:32 +0545

    chore: setup Firebase project, config, and FlutterFire CLI option generation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     id("com.android.application")
+    // Kotlin Android plugin
+    id("org.jetbrains.kotlin.android")
+    // Google Services plugin (Firebase)
+    id("com.google.gms.google-services")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
